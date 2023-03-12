dfae6f3e917439f258441aefa7d25ad58d300056
franki <8024ldbryant@gmail.com>
2023-03-13 01:30:49 +0800

    add 4.2.5.2

---
+++ chapter4/4.2.5.2/test.js
@@ -0,0 +1,41 @@
+import { TaskQueue } from './TaskQueue.js'
+
+function makeSampleTask(name) {
+  return (cb) => {
+    console.log(`${name} started`)
+    setTimeout(() => {
+      console.log(`${name} completed`)
+      cb()
+    }, Math.random() * 2000)
+  }
+}
+
+function task1(cb) {
+  console.log('task1 started')
+  queue
+    .pushTask(makeSampleTask('task1 -> subtask1'))
+    .pushTask(makeSampleTask('task1 -> subtask2'))
+
+  setTimeout(() => {
+    console.log(`task1 completed`)
+    cb()
+  }, Math.random() * 2000)
+}
+
+function task2(cb) {
+  console.log('task2 started')
+  queue
+    .pushTask(makeSampleTask('task2 -> subtask1'))
+    .pushTask(makeSampleTask('task2 -> subtask2'))
+
+  setTimeout(() => {
+    console.log(`task2 completed`)
+    cb()
+  }, Math.random() * 2000)
+}
+
+const queue = new TaskQueue(2)
+
+queue
+  .pushTask(task1)
+  .pushTask(task2)
