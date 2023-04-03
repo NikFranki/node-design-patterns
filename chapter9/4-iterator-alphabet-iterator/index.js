1dec4358446f3a644a34b8fac3270e4c6e4a3071
franki <8024ldbryant@gmail.com>
2023-04-03 20:48:26 +0800

    add iterator-alphabet-iterator

---
+++ chapter9/4-iterator-alphabet-iterator/index.js
@@ -0,0 +1,25 @@
+const A_CHAR_CODE = 65
+const Z_CHAR_CODE = 90
+
+function createAlphabetIterator() {
+  let currCode = A_CHAR_CODE
+
+  return {
+    next() {
+      const currChar = String.fromCharCode(currCode)
+      if (currCode > Z_CHAR_CODE) {
+        return { done: true }
+      }
+
+      currCode++
+      return { done: false, value: currChar }
+    }
+  }
+}
+
+const iterator = createAlphabetIterator()
+let iterationResult = iterator.next()
+while (!iterationResult.done) {
+  console.log(iterationResult.value)
+  iterationResult = iterator.next()
+}
