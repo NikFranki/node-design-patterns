7e95c8ed8d5bc0ed8fd264fc3a779d174eaf9347
franki <8024ldbryant@gmail.com>
2023-04-08 09:09:23 +0800

    add server-side-rendering

---
+++ 10-universal-javascript/8-server-side-rendering/src/frontend/index.js
@@ -0,0 +1,13 @@
+import react from 'react'
+import ReactDOM from 'react-dom'
+import htm from 'htm'
+import { BrowserRouter } from 'react-router-dom'
+
+import { App } from './App.js'
+
+const html = htm.bind(react.createElement)
+
+ReactDOM.render(
+  html`<${BrowserRouter}><${App} /></>`,
+  document.getElementById('root')
+)
