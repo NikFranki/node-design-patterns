46ac3b95f12a82c22d5eacc8d9c1881a0d735b7c
franki <8024ldbryant@gmail.com>
2023-04-22 17:40:11 +0800

    add http-cluster-resilient

---
+++ 12-scalability-and-architectural-patterns/3-http-cluster-resilient/app.js
@@ -0,0 +1,30 @@
+import { createServer } from 'http'
+import { cpus } from 'os'
+import cluster from 'cluster'
+
+
+if (cluster.isMaster) {
+  const availableCpus = cpus()
+  console.log(`Clustering to ${availableCpus.length} processes`)
+  availableCpus.forEach(() => {
+    cluster.fork()
+  });
+  cluster.on('exit', (worker, code) => {
+    if (code !== 0 && !worker.exitAfterDisconnect) {
+      console.log(`Worker ${worker.process.pid} crashed. Starting a new worker`)
+      cluster.fork()
+    }
+  })
+} else {
+  setTimeout(() => {
+    throw new Error('Ooops')
+  }, Math.ceil(Math.random() * 3) * 1000);
+  const { pid } = process
+  const server = createServer((req, res) => {
+    let i = 1e7; while (i > 0) { i-- }
+    console.log(`Handing request from ${pid}`)
+    res.end(`Hello from ${pid}\n`)
+  })
+
+  server.listen(8080, () => console.log(`Started at ${pid}`))
+}
