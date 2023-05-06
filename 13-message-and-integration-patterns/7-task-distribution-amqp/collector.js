3f709523ac216896ad6ceba2706c88eda9cc3e26
franki <8024ldbryant@gmail.com>
2023-05-06 10:00:48 +0800

    add task-distribution-amqp

---
+++ 13-message-and-integration-patterns/7-task-distribution-amqp/collector.js
@@ -0,0 +1,13 @@
+import amqp from 'amqplib'
+
+async function main() {
+  const connection = await amqp.connect('amqp://localhost')
+  const channel = await connection.createChannel()
+  const { queue } = await channel.assertQueue('tasks_queue')
+
+  channel.consume(queue, msg => {
+    console.log(`Message from worker: ${msg.content.toString()}`)
+  })
+}
+
+main().catch(err => console.error(err))
